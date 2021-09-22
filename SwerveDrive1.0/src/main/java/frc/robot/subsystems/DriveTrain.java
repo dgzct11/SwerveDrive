45ec971793552a03ae2cc6a5039c183d631f313c
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2021-09-22 18:38:42 -0500

    deleted almost everything.

--- SwerveDrive1.0/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ SwerveDrive1.0/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -62,14 +62,6 @@ public class DriveTrain extends SubsystemBase {
   public double kiDir = 0;
   public double kdDir = 0.01;
   public double kfDir = 0;
-
-  
-  
-
-  public double kpTh = 0;
-  public double kiTh = 0;
-  public double kdTh = 0;
-  public double kfTh = 0.5;
   public int slotIdx = 0;
   int timeout = 0;
   double errorDeg = 1;
@@ -81,21 +73,10 @@ public class DriveTrain extends SubsystemBase {
   PIDControl directionalPIDRB = new PIDControl(kpDir, kiDir, kdDir);
 
   //circle refers to circular path of rotation when turning
-  public double currentStrafeAngle = 0;
-  public double currentCircleRadius = 0;
-  public boolean spinning = false;
-  public double[] currentCircleCenter = new double[2];
-  public double[] currentWheelDistanceFromCircleCenter = new double[4];
-
-
-
-
-
-  public Odometry odometry;
-
-  public DriveTrain(Odometry od) {
+ 
+  public DriveTrain() {
     //configure sensors for each motor controller to sensor in Falcon500
-    TalonSRXConfiguration directionalConfiguration = new TalonSRXConfiguration();
+    
     
     lfd.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
     lfd.setInverted(true);
@@ -242,142 +223,17 @@ lbt.setInverted(true);
 
 
 
-  public void rotateDriveVelocity(double strafeAngle, double speed, double rotateSpeed){
-    //positive rotate speed is left turn, negative rotate speed is right turn
-    rotateSpeed = 0;
-    
-    double strafeXComponent = -Math.sin(Math.toRadians(strafeAngle))*speed;
-    double strafeYComponent = Math.cos(Math.toRadians(strafeAngle))*speed;
-    double rotationComponent = Constants.rotate_dampaner*rotateSpeed;
-    double[] leftFrontVector = {strafeXComponent-rotationComponent, strafeYComponent-rotationComponent};
-    double[] leftBackVector = {strafeXComponent + rotationComponent, strafeYComponent-rotationComponent};
-    double[] rightFrontVector = {strafeXComponent-rotationComponent, strafeYComponent + rotationComponent};
-    double[] rightBackVector = {strafeXComponent + rotationComponent, strafeYComponent + rotationComponent};
-    double[] angles = {RobotContainer.stickTo360(leftFrontVector[0], leftFrontVector[1]),
-                       RobotContainer.stickTo360(leftBackVector[0], leftBackVector[1]),
-                       RobotContainer.stickTo360(rightFrontVector[0], rightFrontVector[1]),
-                       RobotContainer.stickTo360(rightBackVector[0], rightBackVector[1])};
-    double[] speeds = {RobotContainer.magnitutde(leftFrontVector),
-                       RobotContainer.magnitutde(leftBackVector),
-                       RobotContainer.magnitutde(rightFrontVector),
-                       RobotContainer.magnitutde(rightBackVector)};
-    
-    setDirectionalAnglesPID(angles);
-    setThrustVelocity(speeds);
-    
-  }
 
  
 
-  public void arcDrive(double strafeAngle,double speed, double circleRadius){
-   /*
-    currentCircleRadius = circleRadius;
-    currentStrafeAngle = strafeAngle;
+  
 
-    //when robot spins (pure spinning)
-    if(Math.abs(circleRadius)<Constants.spin_threshold){
-      
-        lfd.setSelectedSensorPosition(Constants.spin_angle*Constants.pos_units_per_degree);
-        lbd.setSelectedSensorPosition(-Constants.spin_angle*Constants.pos_units_per_degree);
-        rfd.setSelectedSensorPosition(-Constants.spin_angle*Constants.pos_units_per_degree);
-        rbd.setSelectedSensorPosition(Constants.spin_angle*Constants.pos_units_per_degree);
-
-        lft.set(ControlMode.PercentOutput, speed);
-        lbt.set(ControlMode.PercentOutput, speed);
-        rft.set(ControlMode.PercentOutput, speed);
-        rbt.set(ControlMode.PercentOutput, speed);
-    
-    }
-    // strafing + turning
-    else if(! (Math.abs(circleRadius) == Double.POSITIVE_INFINITY)){
-        double[] angles_speeds = calcAngles(circleRadius);
-
-        lfd.setSelectedSensorPosition((angles_speeds[0])*Constants.pos_units_per_degree);
-        lbd.setSelectedSensorPosition(angles_speeds[1]*Constants.pos_units_per_degree);
-        rfd.setSelectedSensorPosition(angles_speeds[2]*Constants.pos_units_per_degree);
-        rbd.setSelectedSensorPosition(angles_speeds[3]*Constants.pos_units_per_degree);
-      
-        lft.set(ControlMode.PercentOutput, speed*angles_speeds[4]);
-        lbt.set(ControlMode.PercentOutput, speed*angles_speeds[5]);
-        rft.set(ControlMode.PercentOutput, speed*angles_speeds[6]);
-        rbt.set(ControlMode.PercentOutput, speed*angles_speeds[7]);
-      
-    }
-    //only strafing
-    else{
-        lft.set(ControlMode.PercentOutput, speed);
-        lbt.set(ControlMode.PercentOutput, speed);
-        rft.set(ControlMode.PercentOutput, speed);
-        rbt.set(ControlMode.PercentOutput, speed);
-    }
-   */
-  }
-
-
-  public double[] calcAngles(double circleRadius){
-
-    double[] result = new double[8];
-
-    //line going through robot center in the direction of the strafe angle
-    Line robotCenterLinePerpendicular = new Line(currentStrafeAngle+90,Constants.center);
-    
-    //center of turning circle
-    double[] circleCenter = {Math.cos(Math.toRadians(currentStrafeAngle+90))*circleRadius,  Math.sin(Math.toRadians(currentStrafeAngle+90))*circleRadius};
-    currentCircleCenter = circleCenter;
-
-
-    Line leftFront = new Line(currentStrafeAngle,Constants.leftFrontCenter);
-    Line leftBack = new Line(currentStrafeAngle, Constants.leftBackCenter);
-    Line rightFront = new Line(currentStrafeAngle,Constants.rightFrontCenter);
-    Line rightBack = new Line(currentStrafeAngle, Constants.rightBackCenter);
-
-    //poitns of intersection between each line above and the robot center line.
-    double[] leftFrontP = robotCenterLinePerpendicular.getIntersection(leftFront);
-    double[] leftBackP = robotCenterLinePerpendicular.getIntersection(leftBack);
-    double[] rightFrontP = robotCenterLinePerpendicular.getIntersection(rightFront);
-    double[] rightBackP = robotCenterLinePerpendicular.getIntersection(rightBack);
-
-    //distance between circle center and wheel center
-    double leftFrontRadius = RobotContainer.distance(leftFrontP, circleCenter);
-    double leftBackRadius = RobotContainer.distance(leftBackP, circleCenter);
-    double rightFrontRadius = RobotContainer.distance(rightFrontP, circleCenter);
-    double rightBackRadius = RobotContainer.distance(rightBackP, circleCenter);
-
-
-    currentWheelDistanceFromCircleCenter[0] = leftFrontRadius;
-    currentWheelDistanceFromCircleCenter[1] = leftBackRadius;
-    currentWheelDistanceFromCircleCenter[2] = rightFrontRadius;
-    currentWheelDistanceFromCircleCenter[3] = rightBackRadius;
-
-    double maxRadius  = Math.max(Math.max(Math.max(leftFrontRadius, leftBackRadius),rightFrontRadius),rightBackRadius);
-
-  // result's order is leftF angle (0), leftB angle (1), rightF angle (2), rightB angle(3), 
-  //leftF thrust (4), leftB thrust (5), rightF thrust (6), rightB thrust (7)
-    result[0] = currentStrafeAngle + RobotContainer.to360(Math.toDegrees(Math.atan2(RobotContainer.distance(Constants.leftFrontCenter, leftFrontP), leftFrontRadius))) * (circleRadius>0 ? -1:1);
-    result[1] = currentStrafeAngle + RobotContainer.to360(Math.toDegrees(Math.atan2(RobotContainer.distance(Constants.leftBackCenter, leftBackP), leftBackRadius))) * (circleRadius>0 ? 1:-1);
-    result[2] = currentStrafeAngle + RobotContainer.to360(Math.toDegrees(Math.atan2(RobotContainer.distance(Constants.rightFrontCenter, rightFrontP), rightFrontRadius))) * (circleRadius>0 ? -1:1);
-    result[3] = currentStrafeAngle + RobotContainer.to360(Math.toDegrees(Math.atan2(RobotContainer.distance(Constants.rightBackCenter, rightBackP),rightBackRadius))) * (circleRadius>0 ? 1:-1);
-    
-    //thrust = radius/maxradius * speed
-    result[4] = leftFrontRadius/maxRadius;
-    result[5] = leftBackRadius/maxRadius;
-    result[6] = rightFrontRadius/maxRadius;
-    result[7] = rightBackRadius/maxRadius;
-    return result; 
-  }
+ 
 
   
 
   //setters
-  public void setThrustVelocity(double[] velocities){
-    for(int i = 0; i<4; i++){
-      velocities[i] *= Constants.talon_velocity_per_ms;
-    }
-    lft.set(TalonSRXControlMode.Velocity, velocities[0]);
-    lbt.set(TalonSRXControlMode.Velocity, velocities[1]);
-    rfd.set(TalonSRXControlMode.Velocity,velocities[2]);
-    rbd.set(TalonSRXControlMode.Velocity,velocities[3]);
-  }
+ 
   
   
   public void setThrustSpeeds(double[] speeds){
@@ -501,28 +357,7 @@ lbt.setInverted(true);
   }
 
 
-  public void mod(){
-    if(lfd.getSelectedSensorPosition()/Constants.pos_units_per_degree>360)
-    lfd.setSelectedSensorPosition(lfd.getSelectedSensorPosition()%(360*Constants.pos_units_per_degree));
-   else if(lfd.getSelectedSensorPosition()<0)
-    lfd.setSelectedSensorPosition((360*Constants.pos_units_per_degree+lfd.getSelectedSensorPosition())%(360*Constants.pos_units_per_degree));
-
-   if(lbd.getSelectedSensorPosition()/Constants.pos_units_per_degree>360)
-    lbd.setSelectedSensorPosition(lbd.getSelectedSensorPosition()%(360*Constants.pos_units_per_degree));
-   else if(lbd.getSelectedSensorPosition()<0)
-    lbd.setSelectedSensorPosition((360*Constants.pos_units_per_degree+lbd.getSelectedSensorPosition())%(360*Constants.pos_units_per_degree));
-
-   if(rfd.getSelectedSensorPosition()/Constants.pos_units_per_degree>360)
-    rfd.setSelectedSensorPosition(rfd.getSelectedSensorPosition()%(360*Constants.pos_units_per_degree));
-   else if(rfd.getSelectedSensorPosition()<0)
-     rfd.setSelectedSensorPosition((360*Constants.pos_units_per_degree+rfd.getSelectedSensorPosition())%(360*Constants.pos_units_per_degree));
-
-   if(rbd.getSelectedSensorPosition()/Constants.pos_units_per_degree>360) 
-     rbd.setSelectedSensorPosition(rbd.getSelectedSensorPosition()%(360*Constants.pos_units_per_degree));
-   else if(rbd.getSelectedSensorPosition()<0) 
-    rbd.setSelectedSensorPosition((360*Constants.pos_units_per_degree+rbd.getSelectedSensorPosition())%(360*Constants.pos_units_per_degree));
-
-  }
+  
   
   
   @Override
