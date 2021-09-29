a094b0ee8109085f3eebcfe85a7bc990447b1035
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2021-09-29 09:02:33 -0500

    Fixed Odometry

--- SwerveDrive1.0/src/main/java/frc/robot/Constants.java
+++ SwerveDrive1.0/src/main/java/frc/robot/Constants.java
@@ -58,7 +58,7 @@ public final class Constants {
     public static final double pos_units_per_degree_rf = 72.8171171171;
    
     public static final double pos_units_per_rotation = 16410;
-    public static final double pos_units_per_meter = pos_units_per_degree*3.13297135609;
+    public static final double pos_units_per_meter = pos_units_per_rotation*3.13297135609;
     public static final double left_right_wheel_distance = 0.581;
     public static final double max_pos_units = 6178;
     public static final double front_back_wheel_distance = 0.581;
