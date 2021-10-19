f9d894075fe8a42da33bdfda56a108619aebc473
dgzct11 <60042905+dgzct11@users.noreply.github.com>
2021-10-19 11:48:34 -0500

    Update CatchBall.java

--- SwerveDrive1.0/src/main/java/frc/robot/commands/auto_commands/CatchBall.java
+++ SwerveDrive1.0/src/main/java/frc/robot/commands/auto_commands/CatchBall.java
@@ -7,6 +7,7 @@ package frc.robot.commands.auto_commands;
 import java.util.Currency;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.LimeLight;
@@ -17,31 +18,38 @@ public class CatchBall extends CommandBase {
   /** Creates a new CatchBall. */
   DriveTrain driveTrain;
   Odometry odometry;
-  LimeLight limeLight;
+  LimeLight limelight;
+  double kp;
   public CatchBall(DriveTrain dt, Odometry od, LimeLight l) {
     // Use addRequirements() here to declare subsystem dependencies.
     driveTrain = dt;
     odometry = od;
-    limeLight = l;
+    limelight = l;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    Constants.in_auto = true;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double[] targetPointTime = limeLight.trackBall();
+    double[] targetPointTime = limelight.trackBall();
     
     double speed = RobotContainer.distance(targetPointTime, odometry.currentPosition.point)/targetPointTime[2];
     double strafeAngle = RobotContainer.angleToPoint(odometry.currentPosition.point, targetPointTime);
-    driveTrain.rotateDriveVelocity((strafeAngle-NavXGyro.ahrs.getAngle() + 360)%360, speed, 0);
+    double angleError = Math.min( kp * (Math.abs(limelight.getHorizontalAngleDiff())),0.3) * (limelight.getHorizontalAngleDiff()>0 ? -1:1);
+   
+    driveTrain.rotateDriveVelocity((strafeAngle-NavXGyro.ahrs.getAngle() + 360)%360, speed, angleError);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    Constants.in_auto = false;
+  }
 
   // Returns true when the command should end.
   @Override
